150c05ba53795f8ef190ba36462f1bc92c266121
Muh Roshan <leoparmigiana@gmail.com>
2024-05-04 09:12:03 +0530

    tested particular repo fetching using github api

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -7,11 +7,24 @@ import requests
 
 # Create your tests here.
 
-def fetch_data():
+def fetch_repolist_data():
         query = "hello world"
         github_api_endpoint = f"https://api.github.com/search/repositories?q={query}"
         response = requests.get(github_api_endpoint)
         data = response.json()
         print(data)
         
-fetch_data()
+# uncomment it when needed !
+# fetch_data() 
+
+# -> https://api.github.com/repos/OWNER/REPO to fetch data from particular repository
+
+def fetch_repo_data():
+        query = "Roshan-here/Xhatgpt"
+        github_api_endpint = f"https://api.github.com/repos/{query}"
+        response = requests.get(github_api_endpint)
+        data = response.json()
+        print(data)
+        
+# uncomment it when needed !
+# fetch_repo_data()
